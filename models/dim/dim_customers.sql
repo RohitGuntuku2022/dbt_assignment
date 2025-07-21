0ce5eb485ebf102f911f73804ea1341488a46645
Rohit Guntuku <134839557+RohitGuntuku2022@users.noreply.github.com>
2025-07-22 00:11:28 +0530

    Update dim_customers.sql

--- models/dim/dim_customers.sql
+++ models/dim/dim_customers.sql
@@ -1,6 +1,7 @@
 SELECT
     {{ dbt_utils.generate_surrogate_key(['customer_id']) }} AS customer_key,  -- Surrogate key, preferred for joins
-    customer_id,              
+    customer_id,
+    first_name,
     last_name,
     email,
     signup_date,
